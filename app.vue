4f49e293449b3dac9786a2554bd4bda163ef0b5a
MogliCodes <moglicodes@gmail.com>
2024-07-26 22:28:58 +0200

    chore(deps): add eslint

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <header class="py-4 shadow-lg">
       <div class="container mx-auto">
-        <h1 class="font-bold">NuxtShip</h1>
+        <h1 class="font-bold">
+          NuxtShip
+        </h1>
       </div>
     </header>
   </div>
 </template>
-<script setup lang="ts">
-</script>
