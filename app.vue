07ca4b518193607befea2437429c46c1cf6eb394
MogliCodes <moglicodes@gmail.com>
2024-07-26 22:25:38 +0200

    chore(deps): add testing libraries

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <header class="py-4 shadow-lg">
+      <div class="container mx-auto">
+        <h1 class="font-bold">NuxtShip</h1>
+      </div>
+    </header>
   </div>
 </template>
+<script setup lang="ts">
+</script>
